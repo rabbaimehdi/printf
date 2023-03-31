8473a555be50257699cb986298a660874af2a391
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-03-31 11:14:11 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,1 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _printf(const char *format, ...);
+
+/**
+ * struct buffer_s - A new type defining a buffer struct.
+ * @buffer: A pointer to a character array.
+ * @start: A pointer to the start of buffer.
+ * @len: The length of the string stored in buffer.
+ */
+typedef struct buffer_s
+{
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
+
+buffer_t *init_buffer(void);
+void free_buffer(buffer_t *output);
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
+unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
+
+#endif
